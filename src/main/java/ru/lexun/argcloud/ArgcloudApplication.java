a539acc9ee747b969c051efe5f0ba891cb91fb3d
lexunok <lex48949@gmail.com>
2023-02-06 12:55:40 +0500

    first init

---
+++ src/main/java/ru/lexun/argcloud/ArgcloudApplication.java
@@ -0,0 +1,13 @@
+package ru.lexun.argcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArgcloudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArgcloudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/lexun/argcloud/ArgcloudApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.lexun.argcloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArgcloudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
