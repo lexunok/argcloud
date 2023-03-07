d6af94fc352df60e2cea1c3b9247d52e82f3afb8
Flakoooo <vlasov.vadom@gmail.com>
2023-03-07 16:17:23 +0500

    nav in a separate file

--- src/frontend/src/components/ArgNews.vue
+++ src/frontend/src/components/ArgNews.vue
@@ -1,66 +1,6 @@
 ﻿<template>
   <v-app id="inspire">
-    <v-navigation-drawer expand-on-hover
-                         class="pt-4"
-                         color="grey-lighten-3"
-                         rail>
-      <v-list>
-        <v-list-item link
-                     prepend-avatar="https://lezgigazet.ru/wp-content/uploads/2020/11/nonimg.jpg"
-                     :title="fullname"
-                     :subtitle="username"
-                     to="/profile">
-        </v-list-item>
-      </v-list>
-      <v-divider></v-divider>
-      <v-list density="compact"
-              nav>
-        <v-list-item link
-                     prepend-icon="mdi-newspaper"
-                     title="Новости"
-                     to="/news">
-        </v-list-item>
-        <v-list-item link
-                     prepend-icon="mdi-account-group"
-                     to="/team"
-                     title="Команда">
-          <template v-slot:append>
-            <v-badge color="info"
-                     content="1"
-                     inline>
-            </v-badge>
-          </template>
-        </v-list-item>
-        <v-list-item link
-                     to="/chat"
-                     prepend-icon="mdi-forum"
-                     title="Чат">
-          <template v-slot:append>
-            <v-badge color="error"
-                     content="31"
-                     inline>
-            </v-badge>
-          </template>
-        </v-list-item>
-
-        <v-list-item link
-                     @click="view = 'ArgFiles'"
-                     prepend-icon="mdi-folder"
-                     title="Мои файлы"
-                     to="/files">
-        </v-list-item>
-        <v-list-item link
-                     prepend-icon="mdi-hammer-screwdriver"
-                     title="Инструменты"
-                     to="/tools">
-        </v-list-item>
-        <v-list-item link
-                     prepend-icon="mdi-exit-to-app"
-                     title="Выйти"
-                     to="/">
-        </v-list-item>
-      </v-list>
-    </v-navigation-drawer>
+    <ArgNavigation />
 
     <v-main>
       <div>
@@ -77,7 +17,9 @@
   </v-app>
 </template>
 <script>
-  export default {
+  import ArgNavigation from "./ArgNavigation.vue"
+    export default {
 
-  }
+    components: { ArgNavigation },
+     }
 </script>
